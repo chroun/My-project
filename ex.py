949141068d394edd177e5b2eb87e6cc1b1e2e5ea
NITA.CHROUN <NITA.CHROUN@PNC.LAN>
2025-06-10 10:51:56 +0700

    #

---
+++ ex.py
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>This is my home page</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde rerum nam, officiis repellendus ratione saepe modi praesentium. In iure pariatur inventore. Cupiditate, sequi eaque! Sint, sapiente. Sed assumenda eos eius.</p>
+    <ol>
+        <li><a href="./help.html">help</a></li>
+        <li><a href="./music/song.html">songs</a></li>
+        <li><a href="./movies/cinema.html">cinema</a></li>
+        <li><a href="./movies/action_movies/action.html">action</a></li>
+   
